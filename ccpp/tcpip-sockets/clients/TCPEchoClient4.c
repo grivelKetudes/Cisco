705da82f418b1025846ef904e82db86c64cad3d9
grivelKetudes <8211356+grivelKetudes@users.noreply.github.com>
2024-03-31 20:47:06 +0200

    main-TCPEchoClient4-base_0003

--- ccpp/tcpip-sockets/clients/TCPEchoClient4.c
+++ ccpp/tcpip-sockets/clients/TCPEchoClient4.c
@@ -7,3 +7,20 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <Pratical.h>
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 3 || arc > 4) // Test for correct number of arguments
+    DieWithUserMessage("Parameter(s)",
+        "<Server Address in IPv4> <Echo Word> [<Server Port>]");
+    
+    char *servIP = argv[1]; // First arg's: server IP address (0.0.0.0) pointer *servIP
+    char *echoString = argv[2]; // Second arg's: string to echo
+
+    // Third arg's (optional), is server port (numeric, 7 is well-know echo port)
+    in_port_t servPort = (argc == 4) ? atoi(argv[3]) : 7; // Wise atoi(argv[3]) or 7
+
+    // Create a reliable, stream socket using TCP
+    int sock = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    ...
+}
